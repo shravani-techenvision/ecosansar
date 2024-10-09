862489d8f688fe4a33dd7bbfdff4c1d0eeb9dac4
Radhika <radhika@techenvision.in>
2024-10-09 13:12:34 +0530

    added code
    added code

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -245,7 +245,20 @@
             </div>
         </section>
     @endif
+    <section class="block">
+        <div class="container">
 
+    <ins class="adsbygoogle"
+    style="display:block"
+    data-ad-client="ca-pub-4322110929509521"
+    data-ad-slot="8759068321"
+    data-ad-format="auto"
+    data-full-width-responsive="true"></ins>
+    <script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+    </div>
+    </section>
         <section class="block">
             <div class="container">
                 <div class="center">
@@ -744,20 +757,7 @@
 <!--        </div>-->
 <!--    </div>-->
 <!--</section>-->
-<section class="block">
-    <div class="container">
 
-<ins class="adsbygoogle"
-style="display:block"
-data-ad-client="ca-pub-4322110929509521"
-data-ad-slot="8759068321"
-data-ad-format="auto"
-data-full-width-responsive="true"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-</div>
-</section>
    <section class="block py-5 bg-light">
     <div class="container">
         <div class="section-title text-center mb-5">
